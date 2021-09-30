207ae69ac30ca321725d5cc8e766a32385cbac7e
AV <87459537+OAOImav@users.noreply.github.com>
2021-09-30 18:45:55 +0530

    Secured Token

--- Discord.py/Program Files/Geralt.py
+++ Discord.py/Program Files/Geralt.py
@@ -119,4 +119,5 @@ async def on_message(message: discord.Message):
         await channel.send(embed = emb)
     await bot.process_commands(message)
 
-bot.run('ODczMjA0OTE5NTkzNzMwMTE5.YQ1BdA.HNr2ruzPri47HN3NWoVQ_wkdi54')
+token = json.load(open(r'D:\AV\PC\Coding\Discord Bot\Geralt\Discord.py\Program Files\config.json'))
+bot.run(f'{token["TOKEN"]}')
