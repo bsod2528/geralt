c49244865a00226a09ceca9fc2eb0204196a4975
AV <87459537+OAOImav@users.noreply.github.com>
2021-10-01 09:43:23 +0530

    Secured API Key & changed Info Color

--- Discord.py/Program Files/cogs/utility.py
+++ Discord.py/Program Files/cogs/utility.py
@@ -14,40 +14,37 @@ class Utility(commands.Cog, discord.ui.View):
 
     def __init__ (self, bot):
         self.bot = bot
-    emote = json.load(open('D:\AV\PC\Coding\Discord Bot\Geralt\Discord.py\Program Files\emote.json'))
 
 #---about me---#
-    @commands.command(name = 'info', help = "View some info about the bot", brief = "Get Bot Info")
+    @commands.command(name = "about", help = "View some info about the bot", brief = "Get Bot Info", aliases = ['info', "botinfo"])
     @commands.guild_only()
     async def about(self, ctx):
+        emote = json.load(open('D:\AV\PC\Coding\Discord Bot\Geralt\Discord.py\Program Files\emote.json'))
         dev = self.bot.get_user(750979369001811982)
-        colour = ctx.author.color
-        emb = discord.Embed(
-            title = 'About Me', 
-            description = f'Geralt is an amazing and simple bot to roast your friends and to moderate your server! Down below is the button to my webite. Go check it totally to take a look at all my commands and also to invite me into your server. Geralt has been developed and maintained by ****\n\u200b', 
-            colour = colour)
-        emb.set_thumbnail(url = f'https://cdn.discordapp.com/avatars/873204919593730119/f7fa349c1100489a68a32672e6a55edc.webp?size=128a')
-        emb.add_field(
-            name = 'Written in:', 
-            value = f'**Language:** **[`python 3.8.5`](https://www.python.org/)**\n**Library:** **[`discord.py 2.0`](https://github.com/Rapptz/discord.py)**\nㅤㅤㅤㅤ\U00002514 Master Branch')
-        emb.add_field(
-            name = 'Stats:',
-            value = f'**Servers:** {len([g.id for g in self.bot.guilds])} servers\n**Users:** {len([g.id for g in self.bot.users])}',
-            inline = False)
-        emb.add_field(
-            name = 'On Discord Since',
-            value = f'<t:{round(ctx.me.created_at.timestamp())}:D>')
-        emb.timestamp = datetime.datetime.now(datetime.timezone.utc)
+        colour = discord.Color.from_rgb(117, 128, 219)
+        embed = discord.Embed(title = "Geralt : The Bot", description = f"Geralt is a simple moderation + fun bot to have in your discord server ! You can invite me to your server by going to my website, and join my support server. This bot is made and maintained by **[{dev}]({dev.avatar})**\n\u200b", colour = colour)
+        embed.set_thumbnail(url = ctx.me.avatar)
+        embed.add_field(
+            name = "Coded in:",
+            value=f"**Language:** **[`python 3.9.7`](https://www.python.org/)**\n**Library:** **[`discord.py 2.0`](https://github.com/Rapptz/discord.py)**\nㅤㅤㅤㅤ\U00002514 Master Branch")
+        embed.add_field(
+            name = "Statistics:",
+            value=f"**Servers:** `{len([g.id for g in self.bot.guilds])}`\n**Users:** `{len([g.id for g in self.bot.users])}`", inline = True)
+        embed.add_field(
+            name = "On Discord Since:",
+            value=f"<t:{round(ctx.me.created_at.timestamp())}:D>")
         view = discord.ui.View()
         view.add_item(discord.ui.Button(
             style = discord.ButtonStyle.link, 
             url = 'https://bsod2528.wixsite.com/geralt', 
-            label = 'Geralt | Home ', 
-            emoji = '<:me:881174571804409886>'))
-        view.add_item(discord.ui.Button (
+            label = "Geralt | Home", 
+            emoji = "<:aww:893334504482172979>"))
+        view.add_item(discord.ui.Button(
+            style = discord.ButtonStyle.link,
             label = 'Geralt | Support',
-            url = 'https://discord.gg/JXEu2AcV5Y'))
-        await ctx.send(embed = emb, view = view)
+            url = 'https://discord.gg/JXEu2AcV5Y',
+            emoji = '<:roolessgoo:886952527625609267>'))
+        await ctx.send(embed = embed, view = view)
 
 #---latency---#
     @commands.command(name = 'ping', help = 'Sends out bot latency!', brief = 'Well, nvm!')
@@ -59,9 +56,10 @@ class Utility(commands.Cog, discord.ui.View):
 #---search pic---#
     @commands.command(name = 'pic', help = 'Wanna show an example?', brief = 'Get an image from Google Images!')
     async def pic(self, ctx, *search):
+        apikey = json.load(open(r'D:\AV\PC\Coding\Discord Bot\Geralt\Discord.py\Program Files\config.json'))
         ran = random.randint(0,9)
         resource = build('customsearch', 'v1', developerKey = api_key).cse()
-        result = resource.list(q = f'{search}', cx = 'de0a8ba5eccedba09', searchType = 'image').execute()
+        result = resource.list(q = f'{search}', cx = f'{apikey["API"]}', searchType = 'image').execute()
         url = result['items'][ran]['link']
         emb = discord.Embed(
             title = f'Here is your Image',
@@ -73,12 +71,13 @@ class Utility(commands.Cog, discord.ui.View):
 #---web search---#
     @commands.command(name = 'web', help = 'Wanna find something quickly?', brief = 'Searches Google for you!')
     async def web(self, ctx,*search):
+        apikeyy = json.load(open(r'D:\AV\PC\Coding\Discord Bot\Geralt\Discord.py\Program Files\config.json'))
         page = 3
         resource = build("customsearch", "v1", developerKey=api_key).cse()
         images = []
         strsAPPEND = ""
         for i in range(1, page, 5):
-            result=resource.list(q=f"{search}",cx="de0a8ba5eccedba09",start=i).execute()
+            result=resource.list(q=f"{search}", cx=f'{apikeyy["API"]}',start=i).execute()
             images += result["items"]
     
         for item in images:
