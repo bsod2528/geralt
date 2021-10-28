631477be0aafbd305b2aa7bba94f1dd15498fbb2
BSOD <padmeswari@gmail.com>
2021-10-28 22:34:11 +0530

    Made dir look better, add delete button to errors

--- Program Files/Geralt.py
+++ Program Files/Geralt.py
@@ -1,39 +1,20 @@
-import os
-import webbrowser
-import time
-import random
+import discord, requests, random, sys, asyncio, datetime, logging, json, pathlib, asyncpg, webbrowser, os, time
+import Kernel.Utils.HelpCommand as help
 from discord import activity, channel, mentions, user
 from discord.embeds import EmptyEmbed
 from discord.flags import Intents
 from discord.types.embed import EmbedField
-import requests
-import discord
-import random
-import sys
-import asyncio
-import datetime
-import logging
-import pathlib
-import json
-import asyncpg
-import Utils.HelpCommand as help
-from discord import DMChannel
-from discord import message
+from discord.ui import view
+from google.auth import credentials
+from discord import DMChannel, message
 from discord.enums import ContentFilter, T
-from discord.errors import Forbidden
 from discord.ext import commands
 from discord.ext import tasks
 from discord.utils import find
-from itertools import cycle
 from discord.ext.commands.core import command, has_guild_permissions, has_permissions
-from jishaku.cog import Jishaku
-from jishaku.cog import STANDARD_FEATURES, OPTIONAL_FEATURES
-from jishaku.features.python import PythonFeature
-from jishaku.features.root_command import RootCommand
-from jishaku.features.baseclass import Feature
 from googleapiclient.discovery import build
 from typing import Coroutine, Optional, Union
-
+import Kernel.Utils.ButtonStop as Stop
 from discord import ui, Interaction, SelectOption, ButtonStyle, Embed
 from discord.ext.commands import Cog, Command, Group, DefaultHelpCommand
 	
@@ -44,11 +25,13 @@ class Geralt(commands.Bot):
 			status = discord.Status.idle, 
 			intents = discord.Intents.all(),
 			activity = discord.Game(name = f'.ghelp ; i hate myself'))
-		
+
 	async def on_ready(self):
 		print(f'\n\nCame into life as {self.user} (ID: {self.user.id})')
 		total_members = list(bot.get_all_members())
 		total_channels = sum(1 for x in bot.get_all_channels())
+		chunk_guilds_on_startup = True,
+		case_insensitive = True,
 		print(f'Number of Guilds : {len(bot.guilds)}')
 		print(f'Number of Large Guilds : {sum(g.large for g in bot.guilds)}')
 		print(f'Number of Chunked Guilds : {sum(g.chunked for g in bot.guilds)}')
@@ -69,6 +52,16 @@ logger.addHandler(handler)
 def av(ctx):
 	return ctx.author.id == 750979369001811982 , 760823877034573864
 
+async def on_run():
+	dbkey = {
+		"user"	:	"bsod",
+		"password"	:	"VPVS",
+		"database"	:	"geralt",
+		"host"	:	"127.0.0.1"
+		}
+	db = await asyncpg.create_pool(**dbkey)
+	await db.execute("CREATE TABLE IF NOT EXISTS users(id bigint PRIMARY KEY, data text);")
+	
 class Help(commands.HelpCommand):
 	def get_command_signature(self, command, ctx):
     		return '{0.qualified_name} {0.signature}'.format(command)
@@ -82,7 +75,7 @@ class Help(commands.HelpCommand):
 			emb.description = description
 
 		for cog, commands in mapping.items():
-			name = '<:reply:897151692737486949>Essential' if cog is None else f'<:replygoin:897151741320122458>{cog.qualified_name}'
+			name = '<:reply:897151692737486949>Essential' if cog is None else f'<:reply:897151692737486949>{cog.qualified_name}'
 			filtered = await self.filter_commands(commands, sort = True)
 			if filtered:
 				value = '\u2002'.join(c.name for c in commands)
@@ -95,7 +88,7 @@ class Help(commands.HelpCommand):
 				emb.set_footer(
 					text = 'Run .ginfo for dashboard')
 				emb.timestamp = datetime.datetime.now(datetime.timezone.utc)
-		await self.get_destination().send(embed = emb)
+		await self.get_destination().send(embed = emb, view = Stop.SelfStop())
 	
 	async def send_cog_help(self, cog):
 		embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog), colour=discord.Color.from_rgb(117, 128, 219))
@@ -127,7 +120,7 @@ class Help(commands.HelpCommand):
 					name = self.get_command_signature(command), 
 					value = command.short_doc or 'Short Documentation yet to be provided', 
 					inline = False)
-		await self.get_destination().send(embed=embed)
+		await self.get_destination().send(embed = embed, view = Stop.SelfStop())
 	send_command_help = send_group_help
 	
 	async def on_help_command_error(self, ctx, error):
@@ -135,7 +128,6 @@ class Help(commands.HelpCommand):
 			embed = discord.Embed(title="Error", description=str(error))
 			await ctx.send(embed=embed)
 bot.help_command = Help()
-
 
 for filename in os.listdir(f'{rootdir}/Cogs'):
 	if filename.endswith('.py'):    
@@ -161,5 +153,5 @@ async def on_message(message):
 	await bot.process_commands(message)
 
 
-token = json.load(open(r'Key.json'))
+token = json.load(open(r'Program Files\Key.json'))
 bot.run(f'{token["TOKEN"]}')
