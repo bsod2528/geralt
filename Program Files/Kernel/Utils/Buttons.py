7b3e687b2dbae53e1bb0335f75bcb8f8c067f06a
BSOD <padmeswari@gmail.com>
2021-11-04 18:29:02 +0530

    Fixed Interaction Checks

--- Program Files/Kernel/Utils/Buttons.py
+++ Program Files/Kernel/Utils/Buttons.py
@@ -19,18 +19,17 @@ from discord.user import BU
 class SelfStop(discord.ui.View):
     def __init__(self, bot):
         super().__init__()
+        self.color = 0x2F3136
 
-        @discord.ui.button(label = 'Delete', style = ButtonStyle.blurple, emoji = '\U0001f5d1')
-        async def close(self, button : discord.ui.button, interaction : discord.Interaction, *args) -> bool:
+    @discord.ui.button(label = 'Delete', style = ButtonStyle.blurple, emoji = '\U0001f5d1')
+    async def close(self, button : discord.ui.button, interaction : discord.Interaction, *args) -> bool:
             await interaction.message.delete()
 
     async def interaction_check(self, interaction:discord.Interaction):
         inter_check = discord.Embed(
             color = self.color,
             title = 'SIKE !!',
-            description = f'<@{interaction.user.id}> ! <@{self.help.context.author.id}> INVOKED THE COMMAND, NOT YOU IDIOT !! ',
-            timestamp = self.help.context.message.created_at
-        )
+            description = f'<@{interaction.user.id}> ! YOU DIDNT INVOK THE COMMAND, NOT YOU IDIOT !! ')
         await interaction.response.send_message(embed = inter_check, ephemeral = True)
         return False
         
@@ -38,20 +37,20 @@ class ExceptionButton(discord.ui.View):
     def __init__(self, bot):
         super().__init__()
         
-        @discord.ui.button(label = 'Exception', style = ButtonStyle.danger)
-        async def exception(self, button : discord.ui.button, interaction : discord.Interaction, *args) -> bool:
+    @discord.ui.button(label = 'Exception', style = ButtonStyle.danger)
+    async def exception(self, button : discord.ui.button, interaction : discord.Interaction, *args) -> bool:
             await interaction.response.send_message(f'```py\n{Exception.__class__.__name__} --> {Exception}\n```', ephemeral = True)
 
-        @discord.ui.button(label = 'Delete', style = ButtonStyle.blurple)
-        async def close(self, button : discord.ui.Button, interaction : discord.Interaction, *args) -> bool:
-            await interaction.response.send_message('Deleting', ephemeral = True)
-            await interaction.message.delete()
+    @discord.ui.button(label = 'Delete', style = ButtonStyle.blurple)
+    async def close(self, button : discord.ui.Button, interaction : discord.Interaction, *args) -> bool:
+        await interaction.response.send_message('Deleting', ephemeral = True)
+        await interaction.message.delete()
 
-        @discord.ui.button(label = 'Traceback', style = ButtonStyle.danger)
-        async def traceback(self, button : discord.ui.button, interaction : discord.Interaction, *args) -> bool:
-            stdout = io.StringIO()
-            value = stdout.getvalue()
-            await interaction.response.send_message(f'```py\n{value}{traceback.format_exc()}\n```', ephemeral = True)
+    @discord.ui.button(label = 'Traceback', style = ButtonStyle.danger)
+    async def traceback(self, button : discord.ui.button, interaction : discord.Interaction, *args) -> bool:
+        stdout = io.StringIO()
+        value = stdout.getvalue()
+        await interaction.response.send_message(f'```py\n{value}{traceback.format_exc()}\n```', ephemeral = True)
     
 class Die(discord.ui.View):
     def __init__(self, bot):
@@ -67,6 +66,14 @@ class Die(discord.ui.View):
     async def no(self, button : discord.ui.button, interaction : discord.Interaction):
         await interaction.response.send_message('Thank you for sparing me!', ephemeral = True)
         self.stop()
+
+    async def interaction_check(self, interaction:discord.Interaction):
+        inter_check = discord.Embed(
+            color =  0x2F3136,
+            title = 'SIKE !!',
+            description = f'<@{interaction.user.id}> ! THIS IS A OWNER CMD BEACH !! ')
+        await interaction.response.send_message(embed = inter_check, ephemeral = True)
+        return False
 
 class Nitro(discord.ui.View):
     
