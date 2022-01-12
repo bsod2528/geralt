5149a3f10895c0d05bf13484bfc10343875d4e9d
BSOD <padmeswari@gmail.com>
2022-01-12 21:51:35 +0530

    Added Bot Info

---
+++ Kernel/Utilities/Essential.py
@@ -0,0 +1,41 @@
+import os
+import asyncpg
+
+async def TOTAL_LINES(Path : str, FileType : str = ".py"):
+    Lines = 0
+    for L in os.scandir(Path):
+        if L.is_file():
+            if L.path.endswith(".py"):
+               # Lines += len((await (await aiofiles.open(L.Path, 'r')).read()).split("\n"))
+            #elif L.is_dir():
+                Lines += await TOTAL_LINES(L.Path, FileType)
+    return Lines
+    
+class DB_FUNCS:
+    def __init__(self):
+    
+        async def EXECUTE(self, *ARGS, **KWARGS):
+            """Execute SQL query"""
+            QUERY   =   await asyncpg.pool.Pool.execute(*ARGS, **KWARGS)
+            return QUERY
+        
+        async def EXECUTE_MANY(self, *ARGS, **KWARGS):
+            """Execute multiple SQL queries"""
+            QUERY   =   await asyncpg.pool.Pool.executemany(*ARGS, **KWARGS)
+            return QUERY
+        
+        async def FETCH(self, *ARGS, **KWARGS):
+            """Fetches data from table"""
+            QUERY   =   await asyncpg.pool.Pool.fetch(*ARGS, **KWARGS)
+            return QUERY
+
+        async def FETCH_ROW(self, *ARGS, **KWARGS):
+            """Fetches data from row"""
+            QUERY   =   await asyncpg.pool.Pool.fetchrow(*ARGS, **KWARGS)
+            return QUERY
+        
+        async def FETCH_VALUE(self, *ARGS, **KWARGS):
+            """Fetches data from a given value"""
+            QUERY   =   await asyncpg.pool.Pool.fetchval(*ARGS, **KWARGS)
+            return QUERY
+
---
+++ Kernel/Utilities/Interface.py
@@ -0,0 +1,84 @@
+import pygit2
+import psutil
+import discord
+import datetime
+import itertools
+
+from discord.enums import ButtonStyle
+
+PAIN    =   "This can't be handled by you at the moment, invoke your very own command <:SarahPray:920484222421045258>"
+COLOUR  =   discord.Colour.from_rgb(117, 128, 219)
+
+# Gets latest commits from Github and Format them to make it look sexy :D
+def format_commit(COMMIT):
+    short, _, _     = COMMIT.message.partition("\n")
+    Commit_Desc     = short[0:40] + "..." if len(short) > 40 else short
+    Short_Hash      = COMMIT.hex[0:6]
+    Timezone        = datetime.timezone(datetime.timedelta(minutes = COMMIT.commit_time_offset))
+    Commit_Time     = datetime.datetime.fromtimestamp(COMMIT.commit_time).astimezone(Timezone)
+    Timestamp       = discord.utils.format_dt(Commit_Time, style = 'D')
+    return f"<:GeraltRightArrow:904740634982760459> [**` {Short_Hash} `**](<https://github.com/BSOD2528/Geralt/commit/{COMMIT.hex}>) **: {Commit_Desc} -** [ {Timestamp} ]"
+
+def Latest_Commit(MAX : int = 3):
+    Repository    = pygit2.Repository(".git")
+    Commits = list(itertools.islice(Repository.walk(Repository.head.target, pygit2.GIT_SORT_TOPOLOGICAL), MAX))
+    return "\n".join(format_commit(C) for C in Commits)
+
+# Sub - Class for " Bot Info " command.
+class Info(discord.ui.View):
+    def __init__(self, ctx, bot):
+        super().__init__()
+        self.ctx = ctx
+        self.bot = bot
+        self.Timestamp  =   datetime.datetime.now(datetime.timezone.utc)
+
+    # Shows System Usage at the current moment.
+    @discord.ui.button(label = "System Info", style = ButtonStyle.blurple, emoji = "<a:Info:905750331789561856>")
+    async def SYS_USAGE(self, BUTTON : discord.ui.button, INTERACTION : discord.Interaction):
+        Core_Count  =   psutil.cpu_count()
+        CPU_Usage   =   psutil.cpu_percent()
+        Mem_Per     =   psutil.virtual_memory().percent
+        Mem_GB      =   psutil.virtual_memory().available / 1024 ** 3
+        RAM_Usage   =   psutil.Process().memory_full_info().uss / 1024 ** 2
+
+        SYS_USAGE   =   discord.Embed(
+            title = "<:WinCogs:898591890209910854> System Usage -",
+            description =   f"""```yaml\nCPU Used : {CPU_Usage:.2f} %
+                            \nMemory Used : {RAM_Usage:.2f} %
+                            \nCPU Core Count : {Core_Count} Cores
+                            \nMemory Available : {Mem_GB:.3f} GB [ {Mem_Per} % ]
+                            \nHost OS : Windows 10 Pro 10.0.19042\n```""",
+            colour = COLOUR)
+        SYS_USAGE.timestamp =   self.Timestamp
+        await INTERACTION.response.send_message(embed = SYS_USAGE, ephemeral = True)
+    
+    async def interaction_check(self, INTERACTION : discord.Interaction) -> bool:
+        if INTERACTION.user == self.ctx.author:
+            return True
+        await INTERACTION.response.send_message(content = f"{PAIN}", ephemeral = True)
+    
+    # Get latest Github commits
+    @discord.ui.button(label = "Github Commits", style = ButtonStyle.blurple, emoji = "<a:WumpusVibe:905457020575031358>")
+    async def COMMITS(self, BUTTON : discord.ui.button, INTERACTION : discord.Interaction):
+        COMMIT_EMB  =   discord.Embed(
+            title = "<:WinGIT:898591166864441345> My Latest Changes :",
+            description = f"**[Github](<https://github.com/BSOD2528/Geralt>)** repository if you want to check things out <:verykewl:916903265541689445> \n\n>>> {Latest_Commit(MAX = 3)}",
+            colour = COLOUR)
+        COMMIT_EMB.timestamp = self.Timestamp
+        await INTERACTION.response.send_message(embed = COMMIT_EMB, ephemeral = True)
+    
+    async def interaction_check(self, INTERACTION : discord.Interaction) -> bool:
+        if INTERACTION.user == self.ctx.author:
+            return True
+        await INTERACTION.response.send_message(content = f"{PAIN}", ephemeral = True)
+
+    # Delete the entire message
+    @discord.ui.button(label = "Delete", style = ButtonStyle.danger, emoji = "<a:Trash:906004182463569961>")
+    async def DELETE(self, BUTTON : discord.ui.button, INTERACTION : discord.Interaction):
+        await INTERACTION.response.send_message("Deleting the whole message as you wish", ephemeral = True)
+        await INTERACTION.message.delete()
+   
+    async def interaction_check(self, INTERACTION : discord.Interaction) -> bool:
+        if INTERACTION.user == self.ctx.author:
+            return True
+        await INTERACTION.response.send_message(content = f"{PAIN}", ephemeral = True)
