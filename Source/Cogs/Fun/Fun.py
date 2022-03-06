a0a533fe5abd6a167320047842b58ad318c5f78c
AV <87459537+BSOD2528@users.noreply.github.com>
2022-03-06 09:35:24 +0530

    `pop` command

--- Source/Cogs/Fun/Fun.py
+++ Source/Cogs/Fun/Fun.py
@@ -1,7 +1,7 @@
-from discord import Forbidden
-import disnake
+import discord
 
-from disnake.ext import commands
+from discord import Forbidden
+from discord.ext import commands
 
 import Source.Kernel.Utilities.Crucial as CRUCIAL
 import Source.Kernel.Views.Interface as Interface
@@ -28,11 +28,11 @@ class Fun(commands.Cog):
     @commands.command(
         name    =   "as", 
         brief   =   "Send a Webhook")
-    async def echo(self, ctx, USER : disnake.Member, *, MESSAGE):
+    async def echo(self, ctx, USER : discord.Member, *, MESSAGE):
         """Send a webhook message as the user you mentioned"""
         WBHK = await CRUCIAL.FETCH_WEBHOOK(ctx.channel)
-        thread = disnake.utils.MISSING
-        if isinstance(ctx.channel, disnake.Thread):
+        thread = discord.utils.MISSING
+        if isinstance(ctx.channel, discord.Thread):
             thread = ctx.channel
         await WBHK.send(
             MESSAGE, 
@@ -59,14 +59,14 @@ class Fun(commands.Cog):
         """Get the details of the recently deleted message"""
         try:    
             MESSAGE, AUTHOR, CHANNEL, TIME    =   self.DELETE[ctx.channel.id]    
-            DELETE_EMB   =   disnake.Embed(
+            DELETE_EMB   =   discord.Embed(
                 title   =   "Sniped Deleted Message",
                 description =   f"**<:ReplyContinued:930634770004725821> - [Message Author :]({AUTHOR.display_avatar.url})** {AUTHOR.mention} (`{AUTHOR.id}`)\n**<:ReplyContinued:930634770004725821> - In Channel :** <#{CHANNEL}> (`{CHANNEL}`)\n**<:Reply:930634822865547294> - Message Created At :** {self.bot.DT(TIME)}",
                 colour  =   self.bot.colour)
             DELETE_EMB.add_field(
                 name    =   "Message Content",
                 value   =   f"```prolog\n{MESSAGE}\n```")
-            DELETE_EMB.timestamp    =   disnake.utils.utcnow()
+            DELETE_EMB.timestamp    =   discord.utils.utcnow()
             await ctx.reply(embed = DELETE_EMB, allowed_mentions = self.bot.Mention)
         except:
             await ctx.reply("No one has deleted. any messages as of now <a:HumanBro:905748764432662549>", allowed_mentions = self.bot.Mention)
@@ -81,9 +81,9 @@ class Fun(commands.Cog):
         try:    
             URL, MESSAGE, AUTHOR, CHANNEL, PRE_TIME          =   self.PRE_EDIT[ctx.channel.id]    
             POST_MESSAGE, AUTHOR, CHANNEL, POST_TIME    =   self.POST_EDIT[ctx.channel.id]    
-            View    =   disnake.ui.View()
-            View.add_item(disnake.ui.Button(label = "Jump to Message", style = disnake.ButtonStyle.link, url = URL, emoji = "<a:ChainLink:936158619030941706>"))
-            EDIT_EMB   =   disnake.Embed(
+            View    =   discord.ui.View()
+            View.add_item(discord.ui.Button(label = "Jump to Message", style = discord.ButtonStyle.link, url = URL, emoji = "<a:ChainLink:936158619030941706>"))
+            EDIT_EMB   =   discord.Embed(
                 title   =   "Sniped Edited Message",
                 description =   f"**<:ReplyContinued:930634770004725821> - [Message Author :]({AUTHOR.display_avatar.url})** {AUTHOR.mention} (`{AUTHOR.id}`)\n**<:ReplyContinued:930634770004725821> - In Channel :** <#{CHANNEL}> (`{CHANNEL}`)\n**<:Reply:930634822865547294> - Message Sent At :** {self.bot.DT(PRE_TIME)}",
                 colour  =   self.bot.colour)
@@ -101,27 +101,44 @@ class Fun(commands.Cog):
     @commands.command(
         name    =   "nitro",
         brief   =   "Gift Nitro")
-    async def nitro(self, ctx, *, USER : disnake.Member = None):
+    async def nitro(self, ctx, *, USER : discord.Member = None):
         """Gift a user free nitro!"""
         try:
             if USER is None:
-                NITRO_EMB   =   disnake.Embed(
+                NITRO_EMB   =   discord.Embed(
                 title   =   "<a:Nitro:905661661191479326> Nitro Has Been Gifted!",
                 description =   f">>> {ctx.author.mention} has been gifted nitro classic by {ctx.guild.me.mention} <a:WumpusHypesquad:905661121501990923>.\n<:Reply:930634822865547294> **-** **{ctx.author}** click on the button below to avail the nitro.",
                 colour  =   0x2F3136)
                 NITRO_EMB.set_thumbnail(url = "https://i.imgur.com/w9aiD6F.png")
-                NITRO_EMB.timestamp = disnake.utils.utcnow()    
+                NITRO_EMB.timestamp = discord.utils.utcnow()    
                 await ctx.send(embed = NITRO_EMB, view = Interface.Nitro(USER))
             else:
-                NITRO_EMB   =   disnake.Embed(
+                NITRO_EMB   =   discord.Embed(
                     title   =   "<a:Nitro:905661661191479326> Nitro Has Been Gifted!",
                     description =   f">>> <@{USER.id}> has been gifted nitro classic by {ctx.author.mention} <a:WumpusHypesquad:905661121501990923>.\n<:Reply:930634822865547294> **-** **{USER}** click on the button below to avail the nitro.",
                     colour  =   0x2F3136)
                 NITRO_EMB.set_thumbnail(url = "https://i.imgur.com/w9aiD6F.png")
-                NITRO_EMB.timestamp = disnake.utils.utcnow()
+                NITRO_EMB.timestamp = discord.utils.utcnow()
                 await ctx.send(embed = NITRO_EMB, view = Interface.Nitro(USER))
         except Forbidden as F:
             raise commands.errors(F)
+
+    @commands.command(
+        name    =   "pop",
+        brief   =   "Pop Buttons!")
+    async def pop(self, ctx, *, SIZE : str):
+        """Fidget with the buttons by popping them!"""
+        if SIZE ==  "small":
+            POP_GAME    =   Interface.Pop(ctx)
+            await POP_GAME.start()
+        elif SIZE   ==  "medium":
+            POP_GAME    =   Interface.PopMedium(ctx)
+            await POP_GAME.start()
+        elif SIZE   ==  "large":
+            POP_GAME    =   Interface.PopLarge(ctx)
+            await POP_GAME.start()
+        else:
+            await ctx.reply(f"**{ctx.author}** - please mention a size. Your choices are :\n<:ReplyContinued:930634770004725821> - `small`\n<:ReplyContinued:930634770004725821> - `medium`\n<:Reply:930634822865547294> - `large`.")
 
 def setup(bot):
     bot.add_cog(Fun(bot))
