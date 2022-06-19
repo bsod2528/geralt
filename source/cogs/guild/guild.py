06e381acfc51f6fa07d5a8fd5f99f46b4669ab1d
BSOD2528 <bsod2528@protonmail.com>
2022-06-19 20:48:30 +0530

    Implemented Ticket System

--- source/cogs/guild/guild.py
+++ source/cogs/guild/guild.py
@@ -1,15 +1,22 @@
 import discord
+import asyncio
 import asyncpg as PSQL
 
 from discord.ext import commands
 
 from ...kernel.subclasses.bot import Geralt
+from ...kernel.views.meta import Confirmation
+from ...kernel.subclasses.embed import BaseEmbed
 from ...kernel.subclasses.context import GeraltContext
+from ...kernel.views.tickets import CallTicket, TicketSetup
 
 class Guild(commands.Cog):
     """Manage the guild and my settings."""
     def __init__(self, bot : Geralt):
-        self.bot = bot
+        self.bot : Geralt = bot
+
+        if not self.bot.persistent_views:
+            self.bot.add_view(CallTicket(self.bot, GeraltContext))
     
     @property
     def emote(self) -> discord.PartialEmoji:
@@ -18,18 +25,36 @@ class Guild(commands.Cog):
     async def fetch_prefix(self, message : discord.Message):
         return tuple([prefix["guild_prefix"] for prefix in await self.bot.db.fetch("SELECT guild_prefix FROM custom_prefix WHERE guild_id = $1", message.guild.id)]) or self.bot.default_prefix
 
+    async def dont_archive_and_delete(self, ctx : GeraltContext, ticket_id : int):
+        await self.bot.db.execute("DELETE FROM ticket_kernel WHERE ticket_id = $1 AND guild_id = $2", ticket_id, ctx.guild.id)
+        await ctx.add_nanotick()
+        await asyncio.sleep(5)
+        await ctx.channel.delete()
+
+    async def archive_and_dont_delete(self, ctx : GeraltContext, ticket_id : int):
+        await ctx.channel.edit(name = f"{ctx.channel.name} archived")
+
     @commands.group(
         name = "prefix",
         brief = "Prefix Related Sub-Commands",
         aliases = ["p"])
     @commands.guild_only()
     @commands.has_guild_permissions(administrator = True)
+    @commands.has_guild_permissions(manage_channels = True)
     async def prefix(self, ctx : GeraltContext):
         if ctx.invoked_subcommand is None:
             current_prefix = await self.bot.db.fetchval("SELECT (guild_prefix) FROM custom_prefix WHERE guild_id = $1", ctx.guild.id)
             if not current_prefix:
                 current_prefix = self.bot.default_prefix
-            await ctx.reply(f"My prefix for **{ctx.guild.name}** is `{current_prefix}` <:TokoOkay:898611996163985410>")
+            prefix_emb = BaseEmbed(
+                description = f"`{current_prefix}`\n`.g`\n{ctx.guild.me.mention}",
+                colour = self.bot.colour)
+            prefix_emb.set_footer(text = f"Run {ctx.clean_prefix}help prefix.")
+            if ctx.guild.icon.url:
+                prefix_emb.set_author(name = ctx.guild, icon_url = ctx.guild.icon.url)
+            else:
+                prefix_emb.set_author(name = ctx.guild)
+            await ctx.reply(embed = prefix_emb, mention_author = False)
 
     @prefix.command(
         name = "set",
@@ -60,3 +85,88 @@ class Guild(commands.Cog):
         await self.bot.db.execute("DELETE FROM custom_prefix WHERE guild_id = $1 AND guild_name = $2", ctx.guild.id, ctx.guild.name)
         await ctx.reply(f"Reset prefix back to `{self.bot.default_prefix}` ")
         self.bot.prefixes[ctx.guild.id] = self.bot.default_prefix
+
+    @commands.group(
+        name = "ticket",
+        brief = "Ticket - Tool for you server.",
+        aliases = ["tt", "tools"])
+    @commands.is_owner()
+    @commands.has_guild_permissions(administrator = True)
+    @commands.has_guild_permissions(manage_channels = True)
+    @commands.bot_has_guild_permissions(manage_channels = True)
+    @commands.cooldown(3, 20, commands.BucketType.user)
+    @commands.max_concurrency(1, commands.BucketType.guild)
+    async def ticket(self, ctx : GeraltContext):
+        """Take care of your server by utilising tickets."""
+        if ctx.invoked_subcommand is None:
+            await ctx.command_help()
+    
+    @ticket.command(
+        name = "setup",
+        brief = "Set-up ticket system.")
+    async def ticket_setup(self, ctx : GeraltContext, *, channel : discord.TextChannel = None):
+        """Setup ticket system in your server.
+        ────
+        **Args :** `channel` ─ `#channel` / `send the id` / `type the name`
+        **Example :** `.gticket setup #channel / channel / 123456789012345678`"""
+        if not channel:
+            return await ctx.reply(f"To set up ticket system, you have to `mention` / `send the id` / `type the name of the channel` for me to send the main panel over there.")
+        await TicketSetup(self.bot, ctx, channel).send()
+
+    @ticket.command(
+        name = "close",
+        brief = "Close a ticket.")
+    async def ticket_close(self, ctx : GeraltContext, ticket_id : int = None):
+        """Close a ticket."""
+        if not ticket_id:
+           return await ctx.reply(f"Please pass in the ticket_id to close it.")
+        async def yes(ui : discord.ui.View, interaction : discord.Interaction, button : discord.ui.Button):
+            for view in ui.children:
+                view.disabled = True
+            await interaction.response.defer()
+            await ui.response.edit(content = "Archiving this ticket.", view = ui, allowed_mentions = self.bot.mentions)
+            await self.archive_and_dont_delete(ctx, ticket_id)
+            await ui.response.edit(content = "Archived", view = ui, allowed_mentions = self.bot.mentions)
+        async def no(ui : discord.ui.View, interaction : discord.Interaction, button : discord.ui.Button):
+            for view in ui.children:
+                view.disabled = True
+            await interaction.response.defer()
+            await ui.response.edit(content = f"{ctx.channel.mention} will be deleted in 5 seconds", view = ui, allowed_mentions = self.bot.mentions)
+            await self.dont_archive_and_delete(ctx, ticket_id)
+        Confirmation.response = await ctx.reply("Do you want to archive this ticket?", view = Confirmation(ctx, yes, no), mention_author = False)
+
+    @ticket.command(
+        name = "status",
+        brief = "Status of ticket system.")
+    async def ticket_status(self, ctx : GeraltContext):
+        """Returns the ticket system status for this guild."""
+        fetch_deets = await self.bot.db.fetch("SELECT * FROM ticket_init WHERE guild_id = $1", ctx.guild.id)
+        system_status = [f"> │ ` ─ ` System ID : `{data['id']}`\n> │ ` ─ ` Category ID : `{data['category_id']}`\n> │ ` ─ ` Panel Message ID : [**{data['sent_message_id']}**]({data['jump_url']})" for data in fetch_deets]
+        if not fetch_deets:
+            return await ctx.reply(f"**{ctx.guild}** has not set up Ticket System. To setup, run `{ctx.clean_prefix}ticket`.")
+
+        status_emb = BaseEmbed(
+            title = f"{ctx.guild}'s Ticket System Status",
+            description = "".join(system_status),
+            colour = self.bot.colour)
+        status_emb.set_thumbnail(url = ctx.guild.icon.url)
+        await ctx.reply(embed = status_emb, mention_author = False)
+
+    @ticket.command(
+        name = "dismantle",
+        brief = "Dismantles Ticket System.",
+        aliases = ["delete", "remove", "clear"])
+    async def ticket_dismantle(self, ctx : GeraltContext):
+        """Disables the ticket system in your guild if present."""        
+        try:
+            data = await self.bot.db.fetchval("SELECT (sent_channel_id, sent_message_id) FROM ticket_init WHERE guild_id = $1", ctx.guild.id)
+            await self.bot.http.delete_message(data[0] , data[1])
+            query_one = "DELETE FROM ticket_init WHERE guild_id = $1 "
+            query_two = "DELETE FROM ticket_kernel WHERE guild_id = $1"
+            await self.bot.db.execute(query_one, ctx.guild.id)
+            await self.bot.db.execute(query_two, ctx.guild.id)
+            await ctx.reply(f"Successfully dismantled the `ticket system` from **{ctx.guild}**")
+            await ctx.add_nanotick()
+        except discord.errors.NotFound:
+            return await ctx.reply("It seems you have already executed this command.")
+        await ctx.add_nanotick()
---
+++ source/kernel/views/tickets.py
@@ -0,0 +1,182 @@
+import io
+import typing
+import discord
+import aiohttp
+import traceback
+import asyncpg as PSQL
+
+from ..subclasses.embed import BaseEmbed
+from ..subclasses.bot import Geralt, CONFIG
+from ..subclasses.context import GeraltContext
+
+class SetupTicketPanel(discord.ui.Modal, title = "Setup Your Panel"):
+    def __init__(self, bot : Geralt, ctx : GeraltContext, channel : discord.TextChannel):
+        super().__init__(custom_id = "panel-setup-modal")
+        self.bot : Geralt = bot
+        self.ctx : GeraltContext = ctx
+        self.channel = channel
+
+    category_id = discord.ui.TextInput(
+        label = "Category ID",
+        required = True,
+        placeholder = "ID of the Category for operation.")
+        
+    ticket_emb_description = discord.ui.TextInput(
+        label = "Embed Description",
+        style = discord.TextStyle.paragraph,
+        required = True,
+        placeholder = "Description of the embed for the ticket system.") 
+        
+    async def on_submit(self, interaction : discord.Interaction) -> None:            
+        ticket_emb = BaseEmbed(
+            description = self.ticket_emb_description.value,
+            color = self.bot.colour)
+        ticket_emb.set_thumbnail(url = self.ctx.guild.icon.url)
+        ticket_emb.set_author(name = f"{self.ctx.guild}'s Ticket Panel")
+
+        try:
+            query = "INSERT INTO ticket_init (guild_id, category_id, sent_message_id, sent_channel_id, jump_url, panel_description) VALUES ($1, $2, $3, $4, $5, $6) " \
+                    "ON CONFLICT (id, guild_id) " \
+                    "DO UPDATE SET category_id = $1, panel_description = $5"
+            sent_panel_message = await self.channel.send(embed = ticket_emb, view = CallTicket(self.bot, self.ctx))
+            await self.bot.db.execute(query, self.ctx.guild.id, self.category_id.value.strip(), sent_panel_message.id, self.channel.id, sent_panel_message.jump_url, self.ticket_emb_description.value.strip())
+            id = await self.bot.db.fetchval("SELECT id FROM ticket_init WHERE category_id = $1 and sent_message_id = $2", self.category_id.value.strip(), sent_panel_message.id)
+            await interaction.response.send_message(content = f"Global ID ─ for ticket system for **{self.ctx.guild}** guild is `{id}`", ephemeral = True)
+        except PSQL.UniqueViolationError:
+            content = f"`{self.category_id.value.strip()}` is a value of a category which is already being utilised for taking care of server members tickets." 
+            return await interaction.response.send_message(content = content, ephemeral = True)
+                                                                
+    async def on_error(self, interaction : discord.Interaction, error : Exception) -> None:
+        async with aiohttp.ClientSession() as session:
+            modal_webhook = discord.Webhook.partial(id = CONFIG.get("ERROR_ID"), token = CONFIG.get("ERROR_TOKEN"), session = session)
+            data = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+            try:
+                await modal_webhook.send(content = f"```py\n{data}\n```\n|| Break Point ||")
+            except(discord.HTTPException, discord.Forbidden):
+                await modal_webhook.send(file = discord.File(io.StringIO(data), filename = "Traceback.py"))
+                await modal_webhook.send(content = "|| Break Point ||")
+            await session.close()
+
+class SetupTicketMessage(discord.ui.Modal, title = "Setup Ticket Message"):
+    def __init__(self, bot : Geralt, ctx : GeraltContext):
+        super().__init__(custom_id = "message-setup-modal")
+        self.bot : Geralt = bot
+        self.ctx : GeraltContext = ctx
+    
+    message_description = discord.ui.TextInput(
+        label = "Message Description",
+        required = True,
+        style = discord.TextStyle.paragraph,
+        placeholder = "Enter the description of the message.")
+
+    async def on_submit(self, interaction : discord.Interaction) -> None:
+        query = "UPDATE ticket_init SET message_description = $1 WHERE guild_id = $2"
+        await self.bot.db.execute(query, self.message_description.value.strip(), self.ctx.guild.id)
+
+    async def on_error(self, interaction : discord.Interaction, error : Exception) -> None:
+        async with aiohttp.ClientSession() as session:
+            modal_webhook = discord.Webhook.partial(id = CONFIG.get("ERROR_ID"), token = CONFIG.get("ERROR_TOKEN"), session = session)
+            data = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+            try:
+                await modal_webhook.send(content = f"```py\n{data}\n```\n|| Break Point ||")
+            except(discord.HTTPException, discord.Forbidden):
+                await modal_webhook.send(file = discord.File(io.StringIO(data), filename = "Traceback.py"))
+                await modal_webhook.send(content = "|| Break Point ||")
+            await session.close()
+
+class TicketSetup(discord.ui.View):
+    def __init__(self, bot : Geralt, ctx : GeraltContext, channel : discord.TextChannel):
+        super().__init__(timeout = 180)
+        self.bot : Geralt = bot
+        self.ctx : GeraltContext = ctx
+        self.channel = channel
+        self.remove_item(self.send_message_setup)
+
+    @discord.ui.button(label = "Panel Setup", style = discord.ButtonStyle.grey, emoji = "<:DiscordStaff:905668211163406387>", custom_id = "ticket-panel-modal-invoke")
+    async def send_panel_setup(self, interaction : discord.Interaction, button : discord.ui.Button):
+        await interaction.response.send_modal(SetupTicketPanel(self.bot, self.ctx, self.channel))
+        self.remove_item(self.send_help)
+        self.remove_item(self.delete)
+        self.add_item(self.send_message_setup)
+        self.add_item(self.send_help)
+        self.add_item(self.delete)
+        await interaction.message.edit(view = self)
+
+    @discord.ui.button(label = "Ticket Message Setup", style = discord.ButtonStyle.grey, emoji = "<:AppWindow:987319960059674635>", custom_id = "ticket-message-modal-invoke")
+    async def send_message_setup(self, interaction : discord.Interaction, button : discord.ui.Button):
+        await interaction.response.send_modal(SetupTicketMessage(self.bot, self.ctx))
+        self.send_panel_setup.disabled = True
+        await interaction.message.edit(view = self)
+
+    @discord.ui.button(label = "Help", style = discord.ButtonStyle.green, emoji = "<a:CoffeeSip:907110027951742996>", custom_id = "ticket-help")
+    async def send_help(self, interaction : discord.Interaction, buttom : discord.ui.Button):
+        content = "For `Panel Setup` two arguments have to be filled. They are `Category ID` and `Embed Description`\n> ` ─ ` Category ID : ID of the category in which you want the tickets to be opened up.\n> ` ─ ` Embed Description : What you want to be displayed on the embed of the main system.\n────\n" \
+                  "For `Ticket Message Setup` one argument has to be filled. It is `Message Description`\n> ` ─ ` Message Description : What message you want the user to see upon them going to their ticket channel."
+        await interaction.response.send_message(content = content, ephemeral = True)
+
+    @discord.ui.button(label = "Delete", style = discord.ButtonStyle.red, emoji = "<a:Trash:906004182463569961>", custom_id = "ticket-delete")
+    async def delete(self, interaction : discord.Interaction, button : discord.ui.Button):
+        await interaction.message.delete()
+
+    async def send(self):
+        content = f"Fill up the modal and get ur ticket system going good.\n\n>>> Fill out `Panel Setup` first \U00002757\n" \
+                  f"` ─ ` Panel Setup : To set where the system should be located in {self.ctx.guild} and what it should state.\n" \
+                  f"` ─ ` Ticket Message Setup : To set what I should state once a ticket is opened."
+        self.message = await self.ctx.reply(content = content, view = self)
+        return self.message
+    
+    async def on_timeout(self) -> None:
+        try:
+            for view in self.children:
+                view.disabled = True
+                await self.message.edit(view = self)
+        except discord.errors.NotFound:
+            pass
+
+    async def interaction_check(self, interaction : discord.Interaction) -> bool:
+        pain = f"This view can't be handled by you at the moment, invoke for youself by running `{self.ctx.clean_prefix}{self.ctx.command}` for the `{self.ctx.command}` command <:SarahPray:920484222421045258>"
+        if interaction.user != self.ctx.author:
+            return await interaction.response.send_message(content = f"{pain}", ephemeral = True)
+        if interaction.user == self.ctx.author:
+            return True
+
+class CallTicket(discord.ui.View):
+    def __init__(self, bot : Geralt, ctx : GeraltContext):
+        super().__init__(timeout = None)
+        self.bot : Geralt = bot
+        self.ctx : GeraltContext = ctx
+        
+    @discord.ui.button(label = "Open a ticket", style = discord.ButtonStyle.grey, emoji = "<:Ticket:987172295762149446>", custom_id = "ticket-call-button")
+    async def call_ticket(self, interaction : discord.Interaction, button : discord.ui.Button):    
+        try:
+            fetch_ticket_deets = await self.bot.db.fetch("SELECT * FROM ticket_init WHERE guild_id = $1", interaction.guild.id)
+            category = [data["category_id"] for data in fetch_ticket_deets]
+            message_description = [data["message_description"] for data in fetch_ticket_deets]
+        
+            channel_overwrites : typing.Dict = {
+                interaction.guild.default_role : discord.PermissionOverwrite(read_messages = False, send_messages = False),
+                interaction.user : discord.PermissionOverwrite(read_messages = True, send_messages = True)}
+
+            try:
+                user_channel = await interaction.guild.create_text_channel(
+                    name = f"{interaction.user} Ticket", 
+                    reason = f"As {interaction.user} opened a ticket", 
+                    overwrites = channel_overwrites,
+                    category = discord.Object(id = "".join(map(str,category))))
+                await self.bot.db.execute(f"INSERT INTO ticket_kernel (guild_id, invoker_id, invoked_at) VALUES ($1, $2, $3)", interaction.guild.id, interaction.user.id, interaction.created_at)
+                ticket_id = await self.bot.db.fetchval("SELECT ticket_id FROM ticket_kernel WHERE guild_id = $1 AND invoked_at = $2", interaction.guild.id, interaction.created_at)
+            except discord.HTTPException:
+                return await interaction.response.send_message(content = f"Dear {interaction.user}. There has been an API ratelimit. Please try again in few minutes", ephemeral = True)
+
+            user_channel_emb = BaseEmbed(
+                description = "".join(message_description),
+                colour = self.bot.colour)
+            user_channel_emb.set_thumbnail(url = interaction.guild.icon.url)
+            user_channel_emb.set_footer(text = f"Ticket ID : {ticket_id}")
+            user_channel_emb.set_author(name = f"{interaction.user}'s Ticket Channel", icon_url = interaction.user.display_avatar.url)
+        
+            user_channel = await self.bot.fetch_channel(user_channel.id)
+            await user_channel.send(embed = user_channel_emb)
+            await interaction.response.send_message(content = f"Created a ticket {user_channel.mention} <:NanoTick:925271358735257651>", ephemeral = True)
+        except discord.errors.NotFound:
+            pass
