67553e2f4fbb0225e7e752ed80a513e8cbb629de
BSOD2528 <bsod2528@protonmail.com>
2022-06-23 17:24:50 +0530

    Override jsk

---
+++ source/kernel/utilities/override_jsk.py
@@ -0,0 +1,23 @@
+import discord
+import contextlib
+import jishaku.paginators
+import jishaku.exception_handling
+
+from typing import Union
+from jishaku.meta import __version__
+
+# This method is taken from stella_bot made by InterStella0 [ Github ID ]
+
+async def attempt_add_reaction(msg: discord.Message, reaction: Union[str, discord.Emoji]):
+    reacts = {
+        "\N{WHITE HEAVY CHECK MARK}": "<a:Verify:905748402871095336>",
+        "\N{BLACK RIGHT-POINTING TRIANGLE}": "<:Join:932976724235395072>",
+        "\N{HEAVY EXCLAMATION MARK SYMBOL}": "<a:Nope:988845795920990279>",
+        "\N{DOUBLE EXCLAMATION MARK}": "<:Pain:911261018582306867>",
+        "\N{ALARM CLOCK}": "<a:Time:988848156500451348>"
+    }
+    react = reacts[reaction] if reaction in reacts else reaction
+    with contextlib.suppress(discord.HTTPException):
+        return await msg.add_reaction(react)
+
+jishaku.exception_handling.attempt_add_reaction = attempt_add_reaction
