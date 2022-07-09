45289c9bed38bd93905c669164453c75b3eea6a9
BSOD2528 <bsod2528@protonmail.com>
2022-07-09 09:29:43 +0530

    Hyrbridize prefix cmd and added prefix limit

--- source/cogs/guild/guild.py
+++ source/cogs/guild/guild.py
@@ -1,10 +1,11 @@
-import typing
 import asyncio
+from code import interact
 import asyncpg
 import discord
 
 from discord.ext import commands
 from discord import app_commands
+from typing import Optional, List
 
 from ...kernel.subclasses.bot import Geralt
 from ...kernel.views.meta import Confirmation
@@ -33,6 +34,11 @@ class Guild(commands.Cog):
     async def archive_and_dont_delete(self, ctx: GeraltContext, ticket_id: int):
         await ctx.channel.edit(name = f"{ctx.channel.name} archived")
 
+    async def guild_preifx_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
+        tag_deets = await self.bot.db.fetch("SELECT UNNEST(guild_prefix) FROM prefix WHERE guild_id = $1", interaction.guild_id)
+        names = [f"{deets[0]}" for deets in tag_deets]
+        return [app_commands.Choice(name = names, value = names) for names in names if current.lower() in names]
+
     @commands.hybrid_group(
         name = "prefix",
         brief = "Prefix Related Sub-Commands",
@@ -44,7 +50,7 @@ class Guild(commands.Cog):
     @commands.has_guild_permissions(manage_roles = True)
     @commands.has_guild_permissions(administrator = True)
     @commands.has_guild_permissions(manage_channels = True)
-    async def prefix(self, ctx: GeraltContext) -> typing.Optional[discord.Message]:
+    async def prefix(self, ctx: GeraltContext) -> Optional[discord.Message]:
         """Prefix related commands."""
         if ctx.invoked_subcommand is None:
             prefix_emb = BaseEmbed(
@@ -64,12 +70,16 @@ class Guild(commands.Cog):
         with_app_command = True)
     @app_commands.checks.cooldown(3, 5)
     @commands.cooldown(3, 5, commands.BucketType.user)
-    @app_commands.describe(prefix = "Give in a prefix you want to add.")
+    @app_commands.autocomplete(prefix = guild_preifx_autocomplete)
+    @app_commands.describe(prefix = "Make sure you're not entering the same prefix as shown here!")
     async def prefix_add(self, ctx: GeraltContext, *, prefix: str = None):
         """Add a custom prefix.
         ────
         **Note:** Once a prefix is added, default prefix will not work \U0001f44d
         ────"""
+        total_prefixes = await self.bot.get_prefix(ctx.message)
+        if len(total_prefixes) > 14:
+            return await ctx.reply(f"For **{ctx.guild}** ─ The maximum of `15` prefixes has reached. To add more, please remove other custom prefixes <:SarahYay:990543210235461682>")
         if prefix is None:
             return await ctx.reply("You do realise you have to enter a `new prefix` for that to become the prefix for this guild? <:SarahPout:989816223544012801>")
         if len(prefix) > 15:
@@ -95,23 +105,26 @@ class Guild(commands.Cog):
         with_app_command = True)
     @app_commands.checks.cooldown(3, 5)
     @commands.cooldown(3, 5, commands.BucketType.user)
+    @app_commands.autocomplete(prefix = guild_preifx_autocomplete)
     @app_commands.describe(prefix = "Give in the prefix you want to you remove.")
-    async def prefix_remove(self, ctx: GeraltContext, *, prefix: str = None) -> typing.Optional[discord.Message]:
+    async def prefix_remove(self, ctx: GeraltContext, *, prefix: str = None) -> Optional[discord.Message]:
         """Remove a custom prefix."""
         if not prefix:
-            return await ctx.reply("Pass in a `prefix` for me to set it.")
-        else:
-            try:
-                query = "UPDATE prefix SET guild_prefix = ARRAY_REMOVE(prefix.guild_prefix, $2) WHERE guild_id = $1"
-                await self.bot.db.execute(query, ctx.guild.id, prefix)
-                data = await self.bot.db.fetch("SELECT guild_id, guild_prefix FROM prefix")
-                for guild_id, prefixes in data:
-                    self.bot.prefixes[guild_id] = set(prefixes)
-                await ctx.add_nanotick()
-                await ctx.reply(f"**{ctx.message.author}** - `{prefix}` has been removed to my prefix list for **{ctx.guild.name}** <:SarahLaugh:907109900952420373>")
-            except Exception as exception:
-                await ctx.add_nanocross()
-                return await ctx.send(f"```py\n{exception}\n```")
+            return await ctx.reply("Pass in a `prefix` for me to remove from the list.")
+        try:
+            fetched_prefixes = "\n".join(await self.bot.get_prefix(ctx.message or discord.Interaction.channel))
+            if prefix.strip() not in fetched_prefixes:
+                return await ctx.reply(f"`{prefix}` ─ is not in the guild's prefix list for **{ctx.guild}** <:SarahPout:989816223544012801>")
+            query = "UPDATE prefix SET guild_prefix = ARRAY_REMOVE(prefix.guild_prefix, $2) WHERE guild_id = $1"
+            await self.bot.db.execute(query, ctx.guild.id, prefix)
+            data = await self.bot.db.fetch("SELECT guild_id, guild_prefix FROM prefix")
+            for guild_id, prefixes in data:
+                self.bot.prefixes[guild_id] = set(prefixes)
+            await ctx.add_nanotick()
+            await ctx.reply(f"**{ctx.message.author}** - `{prefix}` has been removed to my prefix list for **{ctx.guild.name}** <:SarahLaugh:907109900952420373>")
+        except Exception as exception:
+            await ctx.add_nanocross()
+            return await ctx.send(f"```py\n{exception}\n```")
     
     @commands.group(
         name = "ticket",
@@ -123,7 +136,7 @@ class Guild(commands.Cog):
     @commands.bot_has_guild_permissions(manage_channels = True)
     @commands.cooldown(3, 20, commands.BucketType.user)
     @commands.max_concurrency(1, commands.BucketType.guild)
-    async def ticket(self, ctx : GeraltContext) -> typing.Optional[discord.Message]:
+    async def ticket(self, ctx : GeraltContext) -> Optional[discord.Message]:
         """Take care of your server by utilising tickets."""
         if ctx.invoked_subcommand is None:
             await ctx.command_help()
@@ -132,7 +145,7 @@ class Guild(commands.Cog):
         name = "setup",
         brief = "Set-up ticket system.")
     @commands.max_concurrency(1, commands.BucketType.guild)
-    async def ticket_setup(self, ctx: GeraltContext, *, channel: discord.TextChannel = None) -> typing.Optional[discord.Message]:
+    async def ticket_setup(self, ctx: GeraltContext, *, channel: discord.TextChannel = None) -> Optional[discord.Message]:
         """Setup ticket system in your server.
         ────
         **Example :** 
@@ -146,7 +159,7 @@ class Guild(commands.Cog):
         name = "close",
         brief = "Close a ticket.")
     @commands.cooldown(3, 20, commands.BucketType.user)
-    async def ticket_close(self, ctx: GeraltContext, ticket_id: int = None) -> typing.Optional[discord.Message]:
+    async def ticket_close(self, ctx: GeraltContext, ticket_id: int = None) -> Optional[discord.Message]:
         """Close a ticket."""
         if not ticket_id:
            return await ctx.reply(f"Please pass in the ticket_id to close it.")
@@ -169,7 +182,7 @@ class Guild(commands.Cog):
         name = "status",
         brief = "Status of ticket system.")
     @commands.cooldown(3, 5, commands.BucketType.user)
-    async def ticket_status(self, ctx: GeraltContext) -> typing.Optional[discord.Message]:
+    async def ticket_status(self, ctx: GeraltContext) -> Optional[discord.Message]:
         """Returns the ticket system status for this guild."""
         try:
             system_status = [f"> │ ` ─ ` System ID : `{self.bot.ticket_init[ctx.guild.id][6]}`\n> │ ` ─ ` Category ID : `{self.bot.ticket_init[ctx.guild.id][0]}`\n> │ ` ─ ` Panel Message ID : [**{self.bot.ticket_init[ctx.guild.id][2]}**]({self.bot.ticket_init[ctx.guild.id][3]})"]
@@ -193,7 +206,7 @@ class Guild(commands.Cog):
         brief = "Dismantles Ticket System.",
         aliases = ["delete", "remove", "clear"])
     @commands.cooldown(2, 10, commands.BucketType.user)
-    async def ticket_dismantle(self, ctx: GeraltContext) -> typing.Optional[discord.Message]:
+    async def ticket_dismantle(self, ctx: GeraltContext) -> Optional[discord.Message]:
         """Disables the ticket system in your guild if present."""        
         try:
             try:
